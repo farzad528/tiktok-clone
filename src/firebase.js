96ef75f30ac16aee963ef578ce5c3038d35238a9
Farzad Sunavala <farzadsunavala@chevron.com>
2020-10-18 21:01:46 -0500

    clean-up

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDyF5MXHhRxSOMZMnbuvf8sq536C8ZH3ck",
+  authDomain: "tiktok-clone-f992e.firebaseapp.com",
+  databaseURL: "https://tiktok-clone-f992e.firebaseio.com",
+  projectId: "tiktok-clone-f992e",
+  storageBucket: "tiktok-clone-f992e.appspot.com",
+  messagingSenderId: "671111845449",
+  appId: "1:671111845449:web:3a9472ceed930f98c15198",
+  measurementId: "G-ZWWEVM6XLW",
+};
