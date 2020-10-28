5781fdcae1b33288459eeadfb81ea7f6234b7c82
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2020-10-28 16:24:58 -0500

    icons

--- src/Video.js
+++ src/Video.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Video.css";
+import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
 
 function Video() {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
   return (
     <div className="video">
-      <video className="video__player" src={"../public/videos/tiktok1.mp4"}></video>
-      <h1>this is a video</h1>
-      {/* VideoFooter /> */}
-      {/* VideoSidebar /> */}
+      <video
+        className="video__player"
+        loop
+        onClick={onVideoPress}
+        ref={videoRef}
+        src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+      ></video>
+      <VideoFooter
+        channel="fullstackfarzzy"
+        description="check out this new build"
+        song="Usher - Yeah"
+      />
+      <VideoSidebar likes={111} messages={222} shares={333} />
     </div>
   );
 }
